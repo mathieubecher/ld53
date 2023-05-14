72561bebc4e7b819f7e97dd71ccb110076bffd1f
Becher <mbelkhenchir@gmail.com>
2023-05-14 17:12:08 +0200

    add exit offset on cinematic for fade out anim

--- Assets/Scripts/UI/Cinematic.cs
+++ Assets/Scripts/UI/Cinematic.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class Cinematic : MonoBehaviour
 {
     [SerializeField] private float m_cinematicDuration= 10.0f;
+    [SerializeField] private float m_exitOffset= 1.5f;
     private CinematicManager m_manager;
     private float m_cooldown;
     public void Play(CinematicManager _manager)
     {
         m_manager = _manager;
         GetComponent<Animator>().SetTrigger("Play");
-        m_cooldown = m_cinematicDuration;
+        m_cooldown = m_cinematicDuration - m_exitOffset;
     }
 
     public void Update()
