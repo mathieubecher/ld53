b02cb900d7ace37d6d129646efa01af8c9004e22
Becher <mbelkhenchir@gmail.com>
2023-05-14 23:09:22 +0200

    Intro Meanwhile 0

--- Assets/Scripts/UI/Cinematic.cs
+++ Assets/Scripts/UI/Cinematic.cs
@@ -8,20 +8,22 @@ public class Cinematic : MonoBehaviour
     [SerializeField] private float m_cinematicDuration= 10.0f;
     [SerializeField] private float m_exitOffset= 1.5f;
     private CinematicManager m_manager;
-    private float m_cooldown;
+    [SerializeField] private float m_cooldown;
+    private bool m_played = false;
     public void Play(CinematicManager _manager)
     {
         m_manager = _manager;
         GetComponent<Animator>().SetTrigger("Play");
-        m_cooldown = m_cinematicDuration - m_exitOffset;
+        m_cooldown = 0.0f;
+        m_played = true;
     }
 
     public void Update()
     {
-        if (m_cooldown > 0.0f)
+        if (m_played && m_cooldown < m_cinematicDuration - m_exitOffset)
         {
-            m_cooldown -= Time.deltaTime;
-            if (m_cooldown <= 0.0f)
+            m_cooldown += Time.deltaTime;
+            if (m_cooldown >= m_cinematicDuration - m_exitOffset)
             {
                 GetComponent<Animator>().SetTrigger("Exit");
             }
