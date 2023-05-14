97fc4bb1e1b0c32cc5118907fda9696a02146ffa
Valdid <valentin-didomenico@orange.fr>
2023-05-14 16:00:54 +0200

    Prepare narration audio and fight music integration

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,20 +2,178 @@ using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
-    static Dictionary<EventReference, Bank> m_eventBankDict;
-    static HashSet<Bank> loadedBanks;
+    private static AudioManager m_instance;
+    private SoundComponent m_soundComponent;
 
-    public static bool TryLoadBankFromEventRef(EventReference _eventReference)
+    public EventReference chapterIntro1;
+    public EventReference chapterMeanwhile1;
+    public EventReference victory1;
+    public EventReference defeat1;
+
+    public EventReference chapterIntro2;
+    public EventReference chapterMeanwhile2;
+    public EventReference victory2;
+    public EventReference defeat2;
+
+    public EventReference chapterIntro3;
+    public EventReference chapterMeanwhile3;
+    public EventReference victory3;
+    public EventReference defeat3;
+
+    public EventReference chapterIntro4;
+    public EventReference chapterMeanwhile4;
+    public EventReference victory4;
+    public EventReference defeat4;
+
+    public EventReference chapterIntro5;
+    public EventReference chapterMeanwhile5;
+    public EventReference victory5;
+    public EventReference defeat5;
+
+    public EventReference fightMusic;
+
+    private int m_curChapIndex;
+
+    private void Awake()
     {
-        bool hasBank;
-        if (m_eventBankDict.ContainsKey(_eventReference))
-            hasBank = true;
+        if (m_instance)
+            Destroy(this);
         else
-            hasBank = false;
-        return hasBank;
+            m_instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        if (!TryGetComponent<SoundComponent>(out m_soundComponent))
+            m_soundComponent = gameObject.AddComponent<SoundComponent>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnChapterIntro(int _index)
+    {
+        m_curChapIndex = _index;
+        switch (_index)
+        {
+            case 0:
+                m_soundComponent.PlaySound(chapterIntro1);
+                break;
+            case 1:
+                m_soundComponent.PlaySound(chapterIntro2);
+                break;
+            case 2:
+                m_soundComponent.PlaySound(chapterIntro3);
+                break;
+            case 3:
+                m_soundComponent.PlaySound(chapterIntro4);
+                break;
+            case 4:
+                m_soundComponent.PlaySound(chapterIntro5);
+                break;
+        }
+    }
+
+    private void OnChapterMeanwhile()
+    {
+        switch (m_curChapIndex)
+        {
+            case 0:
+                m_soundComponent.StopSound(chapterIntro1);
+                break;
+            case 1:
+                m_soundComponent.StopSound(chapterIntro2);
+                break;
+            case 2:
+                m_soundComponent.StopSound(chapterIntro3);
+                break;
+            case 3:
+                m_soundComponent.StopSound(chapterIntro4);
+                break;
+            case 4:
+                m_soundComponent.StopSound(chapterIntro5);
+                break;
+        }
+
+        switch (m_curChapIndex)
+        {
+            case 0:
+                m_soundComponent.PlaySound(chapterMeanwhile1);
+                break;
+            case 1:
+                m_soundComponent.PlaySound(chapterMeanwhile2);
+                break;
+            case 2:
+                m_soundComponent.PlaySound(chapterMeanwhile3);
+                break;
+            case 3:
+                m_soundComponent.PlaySound(chapterMeanwhile4);
+                break;
+            case 4:
+                m_soundComponent.PlaySound(chapterMeanwhile5);
+                break;
+        }
+    }
+
+    private void OnFightStart()
+    {
+        m_soundComponent.PlaySound(fightMusic);
+    }
+
+    private void OnVictory()
+    {
+        m_soundComponent.StopSound(fightMusic);
+
+        switch (m_curChapIndex)
+        {
+            case 0:
+                m_soundComponent.PlaySound(victory1);
+                break;
+            case 1:
+                m_soundComponent.PlaySound(victory2);
+                break;
+            case 2:
+                m_soundComponent.PlaySound(victory3);
+                break;
+            case 3:
+                m_soundComponent.PlaySound(victory4);
+                break;
+            case 4:
+                m_soundComponent.PlaySound(victory5);
+                break;
+        }
+    }
+
+    private void OnDefeat()
+    {
+        m_soundComponent.StopSound(fightMusic);
+
+        switch (m_curChapIndex)
+        {
+            case 0:
+                m_soundComponent.PlaySound(defeat1);
+                break;
+            case 1:
+                m_soundComponent.PlaySound(defeat2);
+                break;
+            case 2:
+                m_soundComponent.PlaySound(defeat3);
+                break;
+            case 3:
+                m_soundComponent.PlaySound(defeat4);
+                break;
+            case 4:
+                m_soundComponent.PlaySound(defeat5);
+                break;
+        }
     }
 }
