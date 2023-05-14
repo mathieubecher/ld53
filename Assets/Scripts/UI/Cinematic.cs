2d301ec94ae4ad27c648cab5239b223b93573766
Becher <mbelkhenchir@gmail.com>
2023-05-14 10:46:30 +0200

    Naration tool

--- Assets/Scripts/UI/Cinematic.cs
+++ Assets/Scripts/UI/Cinematic.cs
@@ -1,14 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cinematic : MonoBehaviour
 {
+    [SerializeField] private float m_cinematicDuration= 10.0f;
     private CinematicManager m_manager;
+    private float m_cooldown;
     public void Play(CinematicManager _manager)
     {
         m_manager = _manager;
         GetComponent<Animator>().SetTrigger("Play");
+        m_cooldown = m_cinematicDuration;
+    }
+
+    public void Update()
+    {
+        if (m_cooldown > 0.0f)
+        {
+            m_cooldown -= Time.deltaTime;
+            if (m_cooldown <= 0.0f)
+            {
+                GetComponent<Animator>().SetTrigger("Exit");
+            }
+        }
     }
 
     public void Next()
